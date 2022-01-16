b0bc2003c003999daac4cf8d9da714221d1998b7
vaddinenisrinadh <srinadhvaddineni@gmail.com>
2022-01-16 19:11:38 +0530

    commit

---
+++ thread/src/thread/Mythread.java
@@ -0,0 +1,15 @@
+package thread;
+public class Mythread extends Thread
+{
+ 	public void run()
+ 	{
+  		System.out.println("concurrent thread started running..");
+}
+ 	public static void main( String args[] )
+ 	{
+  		Mythread mt = new  Mythread();
+  		mt.start();
+ 	}
+}
+
+
