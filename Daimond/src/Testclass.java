6bec8fd6cf47a8ae45a424cfd3ab48d607fb5420
vaddinenisrinadh <srinadhvaddineni@gmail.com>
2022-01-16 18:55:24 +0530

    commit

---
+++ Daimond/src/Testclass.java
@@ -0,0 +1,27 @@
+interface First 
+{  
+    default void show() 
+    { 
+        System.out.println("Default First"); 
+    } 
+} 
+interface Second 
+{  
+    default void show() 
+    { 
+        System.out.println("Default Second"); 
+    } 
+}  
+public class Testclass implements First, Second 
+{  
+    public void show() 
+    {  
+        First.super.show(); 
+        Second.super.show(); 
+    } 
+    public static void main(String args[]) 
+    { 
+        Testclass ob = new Testclass(); 
+        ob.show(); 
+    } 
+}
