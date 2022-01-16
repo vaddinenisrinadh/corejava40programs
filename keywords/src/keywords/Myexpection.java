7fc1e3f11f840cd3496bc8f3c36d2c0f77485c96
vaddinenisrinadh <srinadhvaddineni@gmail.com>
2022-01-16 19:00:19 +0530

    commit

---
+++ keywords/src/keywords/Myexpection.java
@@ -0,0 +1,24 @@
+package keywords;
+
+class MyException extends Exception 
+{ 
+    public MyException(String s) 
+    { 
+        super(s); 
+    } 
+} 
+public class Myexpection 
+{ 
+    public static void main(String args[]) 
+    { 
+        try
+        { 
+            throw new MyException("temp"); 
+        } 
+        catch (MyException ex) 
+        { 
+            System.out.println("Caught"); 
+            System.out.println(ex.getMessage()); 
+        } 
+    } 
+}
